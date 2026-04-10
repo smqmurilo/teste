0eeb8dfd0a729d32d1a2aef7b15622a6403d6711
smqmurilo <murilosimaque22@gmail.com>
2026-04-10 19:19:38 -0300

    a

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './configs/typeorm.config';
+import { UserModule } from './user/user.module';
+import { UsersService } from './users/users.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), UserModule],
+  controllers: [],
+  providers: [UsersService],
 })
 export class AppModule {}
---
+++ src/user/user.reposity.ts
@@ -0,0 +1,5 @@
+mport { EntityRepository, Repository } from 'typeorm';
+import { User } from './user.entity';
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {}
